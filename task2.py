61639467dee78760009b295ef0ce90c1522b1e7d
johndavidmurff41 <john.murff562@students.cmcss.net>
2026-02-11 08:58:04 -0600

    JD

--- task2.py
+++ task2.py
@@ -13,10 +13,10 @@ from data import first, last, birth, days, p_first, p_last, phone, street, city,
 
 
 # birth
-int(birth)
+birth = int(birth)
 
 # days
-int(days)
+days = int(days)
 
 # p_first
 
